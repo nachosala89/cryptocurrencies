795866f80ec1b0bbefb8f27c1c2a3ed82ddbc2bf
nachosala <nachosala89@gmail.com>
2021-11-15 23:17:08 -0300

    Display crypto list on home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import CryptoList from './components/CryptoList';
+import Crypto from './components/Crypto';
 
 const App = () => (
   <>
@@ -10,6 +11,7 @@ const App = () => (
       <Navbar />
       <Routes>
         <Route exact path="/" element={<CryptoList />} />
+        <Route path="/:id" element={<Crypto />} />
       </Routes>
     </Router>
   </>
--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchCryptos } from '../redux/cryptos/cryptos';
 
 const CryptoList = () => {
@@ -13,7 +14,17 @@ const CryptoList = () => {
   }, []);
 
   return (
-    <h1>Cryptos</h1>
+    <ul className="container crypto-list">
+      {(typeof cryptos !== 'undefined')
+        ? cryptos.map((crypto) => (
+          <li key={crypto.id} className="col-6">
+            <Link to={crypto.id}>
+              <h3>{crypto.name}</h3>
+            </Link>
+          </li>
+        ))
+        : <li>Loading...</li>}
+    </ul>
   );
 };
 
